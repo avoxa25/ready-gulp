9d9c2a039d0b158bee9508fde603e87956c321cf
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-03-25 16:49:02 +0300

    Add new features (gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,69 @@
 const gulp = require('gulp');
 
 const plumber = require('gulp-plumber');
-
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
-
-const uglify = require('gulp-uglify');
-
 const rename = require("gulp-rename");
 
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
-const styles = () => {
+const sync = require('browser-sync').create();
+
+const uglify = require('gulp-uglify');
+
+exports.styles = () => {
     return gulp.src('src/*.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(postcss([
-        autoprefixer()
-    ]))
-    .pipe(cleanCss())
-    .pipe(rename((p) => {
-        p.basename += '.min';
-        p.extname = '.css';
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('src'));
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(cleanCss())
+        .pipe(rename((p) => {
+            p.basename += '.min';
+            p.extname = '.css';
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src'))
+        .pipe(sync.stream());
 }
 
-const svgmin = () => {
+exports.imgmin = () => {
     return gulp.src('src/img/*.*')
-    .pipe(imagemin())
-    .pipe(rename((p) => {
-        p.basename += ".min"
-    }))
-    .pipe(gulp.dest('src/img'));
+        .pipe(imagemin())
+        .pipe(rename((p) => {
+            p.basename += ".min"
+        }))
+        .pipe(gulp.dest('src/img'))
+        .pipe(sync.stream());
 }
 
-const toWebp = () => {
+exports.towebp = () => {
     return gulp.src('src/img/*.min.{jpg,png}')
-    .pipe(webp({
-        quality: 50
-    }))
-    .pipe(gulp.dest('src/img'));;
+        .pipe(webp({
+            quality: 50
+        }))
+        .pipe(gulp.dest('src/img'))
+        .pipe(sync.stream());
 }
 
-exports.styles = styles;
-exports.svgmin = svgmin;
-exports.toWebp = toWebp;
+exports.browsersync = (start) => {
+    sync.init({
+        server: {
+            baseDir: 'src'
+        },
+        cors: true,
+        notify: false,
+        ui: false,
+    });
+    start();
+
+    gulp.watch('src/*.scss', gulp.series('styles'));
+    gulp.watch('src/*.html').on('change', sync.reload);
+}
+
